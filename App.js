75bf4420eaecdd85aaafabdf2647be5a1b74fdef
1996-isuru <isurusrimalnt@gmail.com>
2021-06-08 21:06:35 +0530

    adding alert

--- App.js
+++ App.js
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
-import {StyleSheet, View, FlatList} from "react-native";
+import { StyleSheet, View, FlatList, Alert } from "react-native";
 import Header from "./components/header";
 import TodoItem from "./components/todos";
 import AddTodo from "./components/addTodo";
-
 
 export default function App() {
   const [people, setPeople] = useState([
     { name1: "isuru", key: "1" },
     { name1: "srimal", key: "2" },
     { name1: "sandruwan", key: "3" },
-    { name1: "kamal", key: "4" }, 
-    { name1: "nisa-", key: "5" },
+    { name1: "kamal", key: "4" },
+    { name1: "nisal", key: "5" },
   ]);
 
   const pressHandle = (key) => {
     setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.key != key);
+      return prevPeople.filter((person) => person.key != key);
     });
-  }
+  };
 
   const submitHandler = (text) => {
-    setPeople((prevPeople) => {
-      // console.log(text);
-      return [
-        { name1: text, key: Math.random().toString() },
-        ...prevPeople
-      ];
-    })
-  }
+    if (text.length > 3) {
+      setPeople((prevPeople) => {
+        // console.log(text);
+        return [{ name1: text, key: Math.random().toString() }, ...prevPeople];
+      });
+    } else {
+      Alert.alert("OOPS!", "Todo must be over 3 charc longer", [
+        { text: "Undestood", onPress: () => console.log("alert closed") },
+      ]);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -36,7 +38,7 @@ export default function App() {
       <FlatList
         data={people}
         renderItem={({ item }) => (
-          <TodoItem item={item} pressHandle = {pressHandle}/>
+          <TodoItem item={item} pressHandle={pressHandle} />
         )}
       />
       <AddTodo submitHandler={submitHandler} />
@@ -47,7 +49,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     // flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   item: {
     marginTop: 24,
