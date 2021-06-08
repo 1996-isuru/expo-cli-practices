7f606f665151c4c84a78014998c0b9fe5334f5f7
1996-isuru <isurusrimalnt@gmail.com>
2021-06-08 21:36:09 +0530

    keyboard dismiss

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View, FlatList, Alert } from "react-native";
+import { StyleSheet, View, FlatList, Alert, Keyboard, TouchableWithoutFeedback } from "react-native";
 import Header from "./components/header";
 import TodoItem from "./components/todos";
 import AddTodo from "./components/addTodo";
@@ -10,7 +10,7 @@ export default function App() {
     { name1: "srimal", key: "2" },
     { name1: "sandruwan", key: "3" },
     { name1: "kamal", key: "4" },
-    { name1: "nisal", key: "5" },
+    { name1: "nisala", key: "5" },
   ]);
 
   const pressHandle = (key) => {
@@ -33,6 +33,10 @@ export default function App() {
   };
 
   return (
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard');
+    }}>
     <View style={styles.container}>
       <Header />
       <FlatList
@@ -43,6 +47,7 @@ export default function App() {
       />
       <AddTodo submitHandler={submitHandler} />
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
