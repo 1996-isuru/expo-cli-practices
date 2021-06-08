88156d898e20b39a32fe3c9834828a1e5f2d0310
1996-isuru <isurusrimalnt@gmail.com>
2021-06-08 20:53:44 +0530

    totolist adding complete

--- testCodes/todoApp.js
+++ testCodes/todoApp.js
@@ -1,33 +1,29 @@
 import React, { useState } from "react";
-import {StyleSheet, View, FlatList} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import Header from "./components/header";
 import TodoItem from "./components/todos";
-import AddTodo from "./components/addTodo";
 
 
 export default function App() {
   const [people, setPeople] = useState([
     { name1: "isuru", key: "1" },
     { name1: "srimal", key: "2" },
-    { name1: "sandruwan", key: "3" },
-    { name1: "kamal", key: "4" }, 
-    { name1: "nisa-", key: "5" },
+    { name1: "sandaruwan", key: "3" },
+    { name1: "kamal", key: "4" },
+    { name1: "nisal", key: "5" },
   ]);
 
   const pressHandle = (key) => {
     setPeople((prevPeople) => {
       return prevPeople.filter(person => person.key != key);
     });
-  }
-
-  const submitHandler = (text) => {
-    setPeople((prevPeople) => {
-      // console.log(text);
-      return [
-        { name1: text, key: Math.random().toString() },
-        ...prevPeople
-      ];
-    })
   }
 
   return (
@@ -39,7 +35,6 @@ export default function App() {
           <TodoItem item={item} pressHandle = {pressHandle}/>
         )}
       />
-      <AddTodo submitHandler={submitHandler} />
     </View>
   );
 }
